3059873a79badbd102452f755437d33df9f4cb33
Kasen IO <kasenvr@gmail.com>
2020-02-19 15:59:18 -0500

    Add license headers to files.

--- src/background.js
+++ src/background.js
@@ -1,3 +1,13 @@
+/*
+    background.js
+
+    Created by Kalila L. on 15 Dec 2019.
+    Copyright 2020 Project Athena and contributors.
+    
+    Distributed under the Apache License, Version 2.0.
+    See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+*/
+
 'use strict'
 
 import { app, protocol, BrowserWindow, DownloadItem } from 'electron'
